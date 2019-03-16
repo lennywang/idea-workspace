78231b8fa16bdb2e0e0fcd10b477672dbcb2413c
王路路 <wllane@163.com>
2019-03-16 21:16:50 +0800

    多模块

---
+++ springboot3/ch8_springdata/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO person(id,name,age,address) VALUES (hibernate_sequence.nextval,'汪云飞',32,'合肥');
+INSERT INTO person(id,name,age,address) VALUES (hibernate_sequence.nextval,'aa',31,'北京');
+INSERT INTO person(id,name,age,address) VALUES (hibernate_sequence.nextval,'bb',30,'武汉');
+INSERT INTO person(id,name,age,address) VALUES (hibernate_sequence.nextval,'cc',29,'南京');
+INSERT INTO person(id,name,age,address) VALUES (hibernate_sequence.nextval,'dd',28,'长沙');
+INSERT INTO person(id,name,age,address) VALUES (hibernate_sequence.nextval,'ee',27,'合肥');
---
+++ ssh2/readme.md
@@ -0,0 +1,8 @@
+IDEA中Maven项目的web.xml中报错：cannot resolve servlet 'spring-mvc'
+https://blog.csdn.net/hanchao5272/article/details/78992414
+
+IntelliJ IDEA中的web.xml报cannot resolve symbol servlet的错误
+https://blog.csdn.net/onepiece1991/article/details/78548885
+
+使用IDEA创建maven项目整合SSH(多模块聚合)
+https://blog.csdn.net/fighting_sxw/article/details/80850971
